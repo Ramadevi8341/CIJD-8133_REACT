133d4822ed581527dae621fc1876f059be8277c3
Ramadevi8341 <147592018+Ramadevi8341@users.noreply.github.com>
2023-11-03 13:35:22 +0530

    Add files via upload

---
+++ React/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+var parent=<h1>hello world</h1>
+var root=document.getElementById("root")
+var html=ReactDOM.createRoot(root)
+html.render(parent)
